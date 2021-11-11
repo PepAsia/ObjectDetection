2bbf7c8d277eee916cbdeec7a90b2d97a10ab12e
PepAsia <92567258+PepAsia@users.noreply.github.com>
2021-11-11 20:45:52 +0530

    Add files via upload

---
+++ bedroom.js
@@ -0,0 +1,40 @@
+function back()
+{
+   window.location = "index.html";
+}
+img = "";
+
+function preload()
+{
+    img = loadImage("room1.jpg");
+}
+
+function setup()
+{
+    canvas = createCanvas(640,420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : ";
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded");
+    objectDetector.detect(img, gotResults);
+}
+
+function gotResults(error, results)
+{
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+        objects = results;
+    }
+}
+
+function draw()
+{
+    image(img, 0, 0, 640, 420);
+}
---
+++ bottles.js
@@ -0,0 +1,45 @@
+function back()
+{
+   window.location = "index.html";
+}
+
+function back()
+{
+   window.location = "index.html";
+}
+img = "";
+
+function preload()
+{
+    img = loadImage("room4.png");
+}
+
+function setup()
+{
+    canvas = createCanvas(640,420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : ";
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded");
+    objectDetector.detect(img, gotResults);
+}
+
+function gotResults(error, results)
+{
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+        objects = results;
+    }
+}
+
+function draw()
+{
+    image(img, 0, 0, 640, 420);
+}
