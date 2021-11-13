8e95de3f55174bd7aff303b9a6c3a1fe9c40c54c
PepAsia <92567258+PepAsia@users.noreply.github.com>
2021-11-13 15:36:46 +0530

    Add files via upload

--- desk.js
+++ desk.js
@@ -37,7 +37,7 @@ function gotResults(error, results)
         objects = results;
     }
 }
-function draw()
+/*function draw()
 {
     image(img, 0, 0, 640, 420);
     fill("#FF0000");
@@ -51,4 +51,23 @@ function draw()
     noFill();
     stroke("#FF0000");
     rect(90, 10, 500, 350);
+}*/
+function draw()
+{
+    image(img, 0, 0, 640, 420);
+    
+    if(status != "")
+    {
+      for (i=0; i < objects.length; i++)
+      {
+          document.getElementById("status").innerHTML = "Status: objects Detected";
+        fill("#FF0000");
+        percent = floor(objects[i].confidence * 100);
+        text(objects[i].label + " " + percent + "%" , objects[i].x + 15, objects[i].y +15);
+        noFill();
+        stroke("#FF0000");
+        rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+      }
+    }
+   
 }
